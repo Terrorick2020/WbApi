a54b5d336984084b805c26b85036bc3defe4ffe6
unknown <Rick-Sanches2020@yandex.ru>
2025-02-13 00:44:54 +0300

    Добавил документацию

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,6 +1,15 @@
 import knex from 'knex'
 import config from '../../knexfile'
 
+
+/**
+ * Создаёт и экспортирует экземпляр Knex для работы с базой данных.
+ * 
+ * - Использует конфигурацию из `knexfile.ts` (раздел `development`).
+ * - Позволяет выполнять SQL-запросы через Knex в проекте.
+ * 
+ * @constant {Knex} db - Экземпляр Knex, настроенный для среды разработки.
+ */
 const db = knex(config.development)
 
 export default db
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,20 @@ import { fetchAndStoreTariffs } from './service'
 import knex from './db/database'
 import cron from 'node-cron'
 
+/**
+ * Основная функция, выполняющая первоначальное обновление тарифов
+ * и настраивающая крон-задачу для их периодического обновления.
+ * 
+ * Этапы выполнения функции:
+ * 	1. выполняет обновление тарифов, вызывая `fetchAndStoreTariffs(knex)`.
+ * 	2. запускает крон-задачу, которая обновляет тарифы каждый час.
+ * 	3. в случае ошибки выводит сообщение в консоль.
+ * 	4. в конце освобождает ресурсы, закрывая соединение с базой данных.
+ * 
+ * @async
+ * @function main
+ * @returns {Promise<void>}
+ */
 async function main() {
 	try {
 		await fetchAndStoreTariffs(knex)
